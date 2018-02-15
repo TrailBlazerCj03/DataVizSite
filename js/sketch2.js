5c37d7fc51ec83fb7f8c9c922b62be4f011f2abc
Raymond Zhou <Zhoujiacheng@dyn-160-39-209-129.dyn.columbia.edu>
2018-02-15 17:53:26 -0500

    as4_5

--- js/sketch2.js
+++ js/sketch2.js
@@ -1,17 +1,26 @@
 var max_distance;
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(600, 600);
   noStroke();
   max_distance = dist(0, 0, width, height);
 }
 
 function draw() {
   background(255);
+  colorMode(HSB, 100);
   for(var i = 0; i <= width; i += 20) {
     for(var j = 0; j <= height; j += 20) {
       var size = dist(mouseX, mouseY, i, j);
-      size = size/max_distance;
+      size = size/max_distance * 35;
+      strokeWeight(1);
+      stroke("#525252");
+      var distcolor=dist(mouseX, mouseY, i, j);
+      if (distcolor>max_distance){
+        fill(255);
+      } else {
+        fill((distcolor*255)/2000);
+      }
       ellipse(i, j, size, size);
     }
   }
