553467efab88f029671a7efb2c831bccbeb8178c
Raymond Zhou <Zhoujiacheng@dyn-160-39-209-136.dyn.columbia.edu>
2018-03-09 01:43:45 -0500

    assign5

--- js/sketch5.js
+++ js/sketch5.js
@@ -6,28 +6,31 @@ var rightX = 700;
 var textLeft = 30;
 
 
+
 function preload(){
   MNPLUTOTable=loadTable('MN/Asm5.csv','csv','header');
   console.log('Done loading table...');
 }
 
 function setup(){
-  createCanvas(800, 800);
+  createCanvas(1200, 800);
   textSize(12);
   textFont('Roboto');
   console.log('Setup complete...');
   print(MNPLUTOTable.getRowCount() + ' rows loaded...');
   print(MNPLUTOTable.getColumnCount() + ' columns loaded...');
-  var barWidth=(310/MNPLUTOTable.length)*0.8;
-  var barMargin=(310/MNPLUTOTable.length)*0.2;
+
   noLoop();
 }
 
 function drawLabels() {
-  fill(0);
+  fill(100, 102, 153);
   textAlign(LEFT, CENTER);
-  text("Neighborhoods", textLeft - 15, topY - 25);
+  textSize(20);
+
+  text("Neighborhoods: Residential FAR to Commercial FAR", 5, topY - 10);
   textAlign(RIGHT, CENTER);
+
   for (var i = 0; i < 11; i++) {
     noStroke();
     text(i, textLeft, map(i, "MN01", "MN15", bottomY, topY));
@@ -50,17 +53,27 @@ function drawLabels() {
     fill(0);
     noStroke();
     for (var i=0; i<MNPLUTOTable.getRowCount();i++){
-      push();
-      fill('steelblue');
-      translate(0,i*(barWidth+barMargin));
-
-    var Neighborhood=MNPLUTOTable.getString(i,'Neighborhood').split('-')[0];
-    var MN=map(Neighborhood,bottomY,topY);
+      // push();
+      // fill(200);
+      // translate(0,i*(barWidth+barMargin));
+    var barWidth=(310/MNPLUTOTable.length)*0.8;
+    var barMargin=(310/MNPLUTOTable.length)*0.2;
+    var NTAName=MNPLUTOTable.getString(i,'NTAName').split('-')[0];
+    var MN=map(NTAName,bottomY,topY);
     var RC=map(MNPLUTOTable.getNum(i,'R/C'),0,200,leftX,rightX);
 
-    rect(0,0,MNPLUTOTable[i],barWidth);
-    fill('#FFF');
-    text(MNPLUTOTable[i],5,barWidth/2+5);
+    console.log(RC);
+    fill(100, 102, 153);
+    rect(5,80 + i*30,RC*1.5,20);
+    fill('0');
+    text(NTAName,10,95+i*30);
+    fill(100, 102, 153);
+    text(MNPLUTOTable.getNum(i,'R/C'),RC*1.5+10,95+i*30);
   }
   print('New tables created...');
+}
+
+function draw(){
+  drawTables();
+  drawLabels();
 }
